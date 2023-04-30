5caaf75f8d8fba8b036aa862a725f239293cfe72
AbhiGupta143 <mathur1516@gmail.com>
2023-04-30 09:23:27 +0530

    views Added

--- index.js
+++ index.js
@@ -3,44 +3,50 @@ const path = require("path");
 const app = express();
 const port = 5000;
 
+
 // Static Files
 app.use(express.static("static"));
 console.log(__dirname);
+
+const viewPath = path.join(__dirname,"/static/views");
+console.log(viewPath);
 // Specific folder example
 //app.use("/css", express.static(__dirname + "static/css"));
 //app.use("/html", express.static(__dirname + "static/html"));
 //app.use("/public", express.static(__dirname + "static/public"));
 
 // Set View's
-// app.set('views', './views');
-// app.set('view engine', 'ejs');
+ app.set('views', viewPath);
+ app.set('view engine', "hbs");
 
-// Navigation
-app.get("/", (req, res) => {
-  res.sendFile("index.html", { root: __dirname + "/static/html" });
-});
+ app.get("/",(req,res)=>
+ {
+  res.render('index');
+ });
+ 
 app.get("/Register", (req, res) => {
-  res.sendFile("Register.html", { root: __dirname + "/static/html" });
+  res.render("Register");
 });
 app.get("/login", (req, res) => {
-  res.sendFile("login.html", { root: __dirname + "/static/html" });
+  res.render("login");
 });
 app.get("/Error", (req, res) => {
-  res.sendFile("Error.html", { root: __dirname + "/static/html" });
+  res.render("Error");
 });
 app.get("/pricing", (req, res) => {
-  res.sendFile("pricing.html", { root: __dirname + "/static/html" });
+  res.render("pricing");
 });
 app.get("/PrivacyPolicy", (req, res) => {
-  res.sendFile("PrivacyPolicy.html", { root: __dirname + "/static/html" });
+  res.render("PrivacyPolicy");
 });
 app.get("/contact", (req, res) => {
-  res.sendFile("contact.html", { root: __dirname + "/static/html" });
+  res.render("contact");
 });
 app.get("/RememberPassword", (req, res) => {
-  res.sendFile("RememberPassword.html", { root: __dirname + "/static/html" });
+  res.render("RememberPassword");
 });
 app.get("/AboutUs", (req, res) => {
-  res.sendFile("AboutUs.html", { root: __dirname + "/static/html" });
+  res.render("AboutUs.hbs");
 });
+
 app.listen(port, () => console.info(`App listening on port ${port}`));
